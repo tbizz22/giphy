a1e610f4cf947fd3dd633f2fae36ae196b91635e
tyler bisbee <tyler.bisbee@gmail.com>
2018-06-10 16:21:28 -0400

    working local storage

--- Assets/javascript/app.js
+++ Assets/javascript/app.js
@@ -4,26 +4,49 @@ var Key = "zvX6mx15FdjrqCj5yVJQZx8Oox55oEca";
 var buttonTriggered = "";
 var limit = 10;
 var parentalControl = 1;
-var topic = ["dog", "cat", "bird"];
+var topic = [];
+
+// getTopic();
 
 
+function storeTopic() {
+    // localStorage.clear();
+    localStorage.setItem("topic", JSON.stringify(topic));
+}
+
+function getTopic() {
+    if (localStorage.getItem("topic") != null) {
+        topic = JSON.parse(localStorage.getItem("topic"));
+        return topic;
+    } 
+}
+
+getTopic();
 renderTopic();
+
 
 function renderTopic() {
     // get rid of any old buttons
+//    getTopic();
+
     $("#buttonGrid").empty();
     // build the buttons
-    for (i = 0; i < topic.length; i++) {
-        var d = $("<div>");
-        d.addClass("col s12 center-align");
-        var b = $("<button>");
-        b.addClass("btn waves-effect blue-grey lighten-3 black-text buttonStretch hoverable topicButton");
-        b.attr("data-name", topic[i]);
-        b.text(topic[i]);
-        // add the button to the div 
-        $(d).append(b);
-        // add the div to page
-        $("#buttonGrid").append(d);
+    if (topic === null) {
+       return false;
+    } else {
+        for (i = 0; i < topic.length; i++) {
+            var d = $("<div>");
+            d.addClass("col s12 center-align");
+            var b = $("<button>");
+            b.addClass("btn waves-effect blue-grey lighten-3 black-text buttonStretch hoverable topicButton");
+            b.attr("data-name", topic[i]);
+            b.text(topic[i]);
+            // add the button to the div 
+            $(d).append(b);
+            // add the div to page
+            $("#buttonGrid").append(d);
+        }
+        storeTopic();
     }
 }
 
@@ -87,8 +110,8 @@ function getResults(qurl) {
         for (i = 0; i < result.data.length; i++) {
             var rating = result.data[i].rating;
             var title = result.data[i].title;
-            var stillURL = result.data[i].images.fixed_width_still.url
-            var gifURL = result.data[i].images.fixed_width.url
+            var stillURL = result.data[i].images.fixed_height_still.url
+            var gifURL = result.data[i].images.fixed_height.url
 
 
             // begin making divs
