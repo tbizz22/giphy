a5870d2c2f5590a10b45add657c775fabb05829d
tyler bisbee <tyler.bisbee@gmail.com>
2018-06-10 14:04:41 -0400

    working dynamic creation of results

--- Assets/javascript/app.js
+++ Assets/javascript/app.js
@@ -1,23 +1,24 @@
-var URL ="https://api.giphy.com/v1/gifs/search";
+var URL = "https://api.giphy.com/v1/gifs/search";
 // this should not be here :(
-var Key ="zvX6mx15FdjrqCj5yVJQZx8Oox55oEca";
-var buttonTriggered ="";
-var limit = 10
-var topic = ["dog","cat","bird"];
+var Key = "zvX6mx15FdjrqCj5yVJQZx8Oox55oEca";
+var buttonTriggered = "";
+var limit = 10;
+var parentalControl = 1;
+var topic = ["dog", "cat", "bird"];
 
 
 renderTopic();
 
-function renderTopic () {
+function renderTopic() {
     // get rid of any old buttons
     $("#buttonGrid").empty();
     // build the buttons
-    for (i=0; i < topic.length; i++) {
+    for (i = 0; i < topic.length; i++) {
         var d = $("<div>");
         d.addClass("col s12 center-align");
         var b = $("<button>");
-        b.addClass("btn waves-effect blue-grey lighten-3 black-text buttonStretch");
-        b.attr("data-name",topic[i]);
+        b.addClass("btn waves-effect blue-grey lighten-3 black-text buttonStretch hoverable");
+        b.attr("data-name", topic[i]);
         b.text(topic[i]);
         // add the button to the div 
         $(d).append(b);
@@ -27,15 +28,15 @@ function renderTopic () {
 }
 
 
-$("#addIcon").on("click", function(event) {
+$("#submit").on("click", function (event) {
     event.preventDefault();
-    buttonTriggered = $("#icon_prefix").val().trim();
+    buttonTriggered = $("#newTopic").val().trim();
     console.log(buttonTriggered)
-    addTopic();
+    addTopic();    
 })
 
 
-function addTopic () {
+function addTopic() {
     topic.unshift(buttonTriggered);
     renderTopic();
 }
@@ -43,21 +44,73 @@ function addTopic () {
 
 
 
-$(document).on("click", "button", function(event) {
+$(document).on("click", "button", function (event) {
     event.preventDefault();
     var queryTerm = $(this).attr("data-name");
     var queryString = URL + "?q=" + queryTerm + "&api_key=" + Key + "&limit=" + limit;
     var queryURL = encodeURI(queryString);
     console.log(queryURL);
     getResults(queryURL);
+    updateBreadCrumb(queryTerm);
 })
 
 
 function getResults(qurl) {
-    $.ajax ({
+    $.ajax({
         url: qurl,
         method: "GET"
-    }).then(function(response){
-        console.log(response);
+    }).then(function (response) {
+
+        //prepare the grid
+
+        $("#imageGrid").empty();
+        
+        var result = response; // console.log(response);   
+        // for each item in data repsonse 
+        for (i = 0; i < result.data.length; i++) {
+            var rating = result.data[i].rating;
+            var title = result.data[i].title;
+            var stillURL = result.data[i].images.fixed_width_still.url
+            var gifURL = result.data[i].images.fixed_width.url
+
+
+            // begin making divs
+            var colDiv = $("<div>");
+            colDiv.addClass("col s12 m6 l3");
+
+            var cardContainerDiv = $("<div>");
+            cardContainerDiv.addClass("card cardContainer");
+
+            var imageDiv = $("<div>");
+            imageDiv.addClass("card-image cardImg img-responsive")
+
+            var image = $("<img>")
+            image.attr("data-still", stillURL);
+            image.attr("data-gif", gifURL);
+            image.attr("src", stillURL);
+            image.attr("data-state", "still");
+
+            var ratingSpan = $("<span>");
+            ratingSpan.text("Rating: " + rating);
+            ratingSpan.addClass("cardRating");
+
+            var titleSpan = $("<span>");
+            titleSpan.text(title);
+            titleSpan.addClass("card-title");
+
+            // // put it all together...           
+
+            colDiv.append(cardContainerDiv);
+            cardContainerDiv.append(imageDiv);
+            imageDiv.append(image);
+            imageDiv.append(ratingSpan);
+            imageDiv.append(titleSpan);
+            $("#imageGrid").append(colDiv)
+        }
     })
+}
+
+
+function updateBreadCrumb (qt) {
+    $("#searchTerm").text(qt);
 }
